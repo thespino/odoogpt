daefd46d7a96221caf10a1d3dc5d19dbb9d81b55
thespino <2001giorgio@gmail.com>
2023-01-07 19:12:57 +0100

    odoogpt: enchance utils

--- models/odoogpt_openai_utils.py
+++ models/odoogpt_openai_utils.py
@@ -1,48 +1,57 @@
 # -*- coding: utf-8 -*-
 
-import json
-from odoo import models, fields, _
+from odoo import models, _
 from odoo.exceptions import UserError
 import openai
 
 
-class OdoogptOpenaiInteraction(models.AbstractModel):
+class OdoogptOpenaiUtils(models.AbstractModel):
     _name = 'odoogpt.openai.utils'
     _description = 'OdooGPT OpenAI Utils'
 
 
-    PARAMETERS = [
-        # Odoo name, OpenAI name
-        ('odoogpt_openai_api_key', 'api_key'),
-        ('odoogpt_openai_model', 'model'),
-        ('odoogpt_openai_max_tokens', 'max_tokens'),
-        ('odoogpt_openai_temperature', 'temperature'),
-    ]
+    # UTILS ==============================================================================
 
+    def _odoogpt_check_api_key(self, raise_err=True):
+        """Check api key existance in company"""
+        if self.env.company.odoogpt_openai_api_key:
+            return self.env.company.odoogpt_openai_api_key
+        if raise_err:
+            raise UserError(_('OpenAI Api Token not set in settings!'))
+        else:
+            return False
 
-    def _get_parameters(self):
+    def _odoogpt_get_parameters(self, PARAMETERS):
         """Get params to pass to openai library to make a successful request"""
         params = {}
-        for PARAM in self.PARAMETERS:
+        for PARAM in PARAMETERS:
             params[PARAM[1]] = self.env.company[PARAM[0]]
 
         return params
     
-    def _check_token(self):
-        if not self.env.company.odoogpt_openai_api_key:
-            raise UserError(_('OpenAI Api Token not set in settings!'))
 
+    # COMPLETITION =======================================================================
+
+    def _completition_create__get_parameters(self):
+        return self._odoogpt_get_parameters([
+            # Odoo name, OpenAI name
+            ('odoogpt_openai_api_key', 'api_key'),
+            ('odoogpt_openai_model', 'model'),
+            ('odoogpt_openai_max_tokens', 'max_tokens'),
+            ('odoogpt_openai_temperature', 'temperature'),
+        ])
 
     def completition_create(self, prompt, **kwargs):
         """Create a Completition. Ref. https://beta.openai.com/docs/api-reference/completions"""
         response = openai.Completion.create(
             prompt=prompt,
             **{
-                **self._get_parameters(),
+                **self._completition_create__get_parameters(),
                 **kwargs,
             }
         )
 
+        # Log interaction
         self.env['odoogpt.openai.interaction'].sudo().create({
             'prompt': prompt,
             'response_raw': str(response),
