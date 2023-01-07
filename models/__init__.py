ed772ec3e0f12eed5ccf30477a9ecb6226d2ca5f
thespino <2001giorgio@gmail.com>
2023-01-07 19:10:30 +0100

    odoogpt: add models

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,5 @@ from . import res_company
 from . import res_config_settings
 from . import mail_channel
 from . import odoogpt_openai_utils
+from . import odoogpt_openai_model
 from . import odoogpt_openai_interaction
---
+++ models/odoogpt_openai_model.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, _
+
+
+class OdoogptOpenaiModel(models.Model):
+    _name = 'odoogpt.openai.model'
+    _description = 'OdooGPT OpenAI Model'
+    _rec_name = 'openai_id'
+
+    _sql_constraints = [
+        ('UNIQUE_OPENAI_ID', 'UNIQUE(openai_id)', _('The OpenAI Id of a Model must be unique!'))
+    ]
+
+
+    openai_id = fields.Char(
+        string='OpenAI Id',
+        required=False,
+        default=False,
+        index=True,
+    )
+
+    openai_owned_by = fields.Char(
+        string='OpenAI Owned by',
+        required=False,
+        default=False,
+    )
+
+    openai_permissions = fields.Json(
+        string='OpenAI permissions',
+        required=False,
+        default=False,
+    )
+
+
+    # UTILS
+    def _get_models_as_dict(self, domain=[]):
+        """Get all models in a dict format (openai_id: record)"""
+        models = self.search(domain)
+
+        return {model.openai_id: model for model in models}
