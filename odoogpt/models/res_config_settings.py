3d4c11599e3cb7f829cce145d2ab3e8a3257cfd4
thespino <2001giorgio@gmail.com>
2023-01-08 18:41:00 +0100

    odoogpt_base: create module, move stuff

--- odoogpt/models/res_config_settings.py
+++ odoogpt/models/res_config_settings.py
@@ -7,43 +7,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
 
-    odoogpt_openai_api_key = fields.Char(
-        string='OpenAI Api token',
-        help="""Open AI Api token can be found at https://beta.openai.com/account/api-keys""",
-        related='company_id.odoogpt_openai_api_key',
-        default=False,
-        required=False,
-        readonly=False,
-    )
-
-    odoogpt_openai_model = fields.Char(
-        string='OpenAI Model',
-        help="""Open AI Model to use: https://beta.openai.com/docs/models/overview""",
-        related='company_id.odoogpt_openai_model',
-        default='text-davinci-003',
-        required=True,
-        readonly=False,
-    )
-
-    odoogpt_openai_max_tokens = fields.Integer(
-        string='OpenAI Max tokens',
-        help="""See https://beta.openai.com/docs/api-reference/completions/create#completions/create-max_tokens""",
-        related='company_id.odoogpt_openai_max_tokens',
-        default=150,
-        required=True,
-        readonly=False,
-    )
-
-    odoogpt_openai_temperature = fields.Integer(
-        string='OpenAI Temperature',
-        help="""See https://beta.openai.com/docs/api-reference/completions/create#completions/create-temperature""",
-        related='company_id.odoogpt_openai_temperature',
-        default=1,
-        required=True,
-        readonly=False,
-    )
-
-
     odoogpt_openai_prompt_prefix = fields.Char(
         string='OpenAI Prompt prefix',
         help="""Prefix to send to all OpenAI Completition Api requests""",
@@ -65,25 +28,15 @@ class ResConfigSettings(models.TransientModel):
 
     def set_values(self):
         res = super().set_values()
-        self.env.company.odoogpt_openai_api_key = self.odoogpt_openai_api_key
-        self.env.company.odoogpt_openai_model = self.odoogpt_openai_model
-        self.env.company.odoogpt_openai_max_tokens = self.odoogpt_openai_max_tokens
-        self.env.company.odoogpt_openai_temperature = self.odoogpt_openai_temperature
+        self.env.company.odoogpt_openai_prompt_prefix = self.odoogpt_openai_prompt_prefix
+        self.env.company.odoogpt_openai_prompt_suffix = self.odoogpt_openai_prompt_suffix
         return res
 
     @api.model
     def get_values(self):
         res = super().get_values()
         res.update(
-            odoogpt_openai_api_key = self.env.company.odoogpt_openai_api_key,
-            odoogpt_openai_model = self.env.company.odoogpt_openai_model,
-            odoogpt_openai_max_tokens = self.env.company.odoogpt_openai_max_tokens,
-            odoogpt_openai_temperature = self.env.company.odoogpt_openai_temperature
+            odoogpt_openai_prompt_prefix = self.env.company.odoogpt_openai_prompt_prefix,
+            odoogpt_openai_prompt_suffix = self.env.company.odoogpt_openai_prompt_suffix
         )
         return res
-
-
-    def odoogpt_openai_model_select_from_db(self):
-        """Get Models from OpenAI api and show selector wizart"""
-        self.env['odoogpt.openai.model'].sudo().odoogpt_openai_model_refresh_from_api(format='model')
-        return self.env.ref('odoogpt.odoogpt_openai_model_select_wizard_act_window').read()[0]
---
+++ odoogpt_base/models/__init__.py
@@ -0,0 +1,5 @@
+from . import res_company
+from . import res_config_settings
+from . import odoogpt_openai_utils
+from . import odoogpt_openai_model
+from . import odoogpt_openai_log
