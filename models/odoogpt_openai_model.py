b49cef6718172226936ef2a465a6fdc56ae1f588
Giorgio Spinoni <50173895+thespino@users.noreply.github.com>
2023-01-08 15:29:14 +0100

    Revert "odoogpt: add string field to well-print json"
    This reverts commit 031df5f1900e089767d8c18eabdb0959a8ad474c.

--- models/odoogpt_openai_model.py
+++ models/odoogpt_openai_model.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
-import json
 
 
 class OdoogptOpenaiModel(models.Model):
@@ -32,21 +31,6 @@ class OdoogptOpenaiModel(models.Model):
         required=False,
         default=False,
     )
-    openai_permissions_string = fields.Text(
-        string='OpenAI permissions',
-        required=False,
-        default=False,
-        store=False,
-        compute='_compute_openai_permissions_string'
-    )
-
-
-    def _compute_openai_permissions_string(self):
-        for openai_model in self:
-            openai_model.openai_permissions_string = json.dumps(
-                self.openai_permissions or {},
-                indent=4
-            )
 
 
     # UTILS
