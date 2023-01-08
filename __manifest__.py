2605323791df0da6d1b501495c652700f72af856
thespino <2001giorgio@gmail.com>
2023-01-08 13:58:41 +0100

    odoogpt: add openai interactions views

--- __manifest__.py
+++ __manifest__.py
@@ -23,6 +23,7 @@
 
         'views/res_config_settings.xml',
         'views/odoogpt_openai_model.xml',
+        'views/odoogpt_openai_interaction.xml',
 
         'wizard/odoogpt_openai_model_select_wizard.xml',
     ],
