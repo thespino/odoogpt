b29c4397211cccba1f7352171261ac630e033e42
thespino <2001giorgio@gmail.com>
2023-01-14 22:13:28 +0100

    odoogpt_fine_tune: add module

---
+++ odoogpt_fine_tune/README.md
@@ -0,0 +1,8 @@
+# odoogpt_fine_tune
+
+Fine tune OpenAI models
+
+
+## Requirements
+
+See module `odoogpt_base`
---
+++ odoogpt_fine_tune/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'OdooGPT fine tune',
+    'summary': 'Fine tune OpenAI models',
+    'description': """Fine tune OpenAI models""",
+    'license': 'LGPL-3',
+    'version': '0.0.1',
+    'category': 'Productivity',
+    'author': 'thespino',
+    'website': 'https://github.com/thespino',
+
+    'depends': [
+        'odoogpt_base',
+        'odoogpt_files',
+    ],
+
+    'data': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
