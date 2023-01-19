a003cb4d6fbb6c920b054b261920f31d485a076d
thespino <2001giorgio@gmail.com>
2023-01-19 21:35:57 +0100

    all modules: prices

--- odoogpt_files/__manifest__.py
+++ odoogpt_files/__manifest__.py
@@ -5,7 +5,7 @@
     'description': """Manage OpenAI Files""",
     'license': 'LGPL-3',
     'version': '0.0.4',
-    'price': '19.99',
+    'price': '4.99',
     'category': 'Productivity',
     'author': 'thespino',
     'website': 'https://github.com/thespino',
